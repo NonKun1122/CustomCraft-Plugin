07a8220d5ff045e3d0d41f746147b8d3841d0512
NonKun Ch <116896756+NonKun1122@users.noreply.github.com>
2025-11-24 22:06:54 +0700

    Add ChatUtil class for sending colored messages

---
+++ src/main/java/com/nonkungch/customcraft/util/ChatUtil.java
@@ -0,0 +1,10 @@
+package nonkungch.customcraft.util;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+public class ChatUtil {
+    public static void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6[CustomCraft] " + message));
+    }
+}
